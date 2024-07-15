e1e12e6c58ff3357f0afb86ea0510d663dca7a2d
Teodora <vestale.teodora00@gmail.com>
2024-07-15 13:38:59 +0300

    feat: class to functional based + protected layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,12 +1,38 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import Navbar from '@/components/navs/Navbar';
 import Footer from '@/components/footers/AdminFooter';
+import { useUser } from '@/context/UserContext';
+import { useRouter } from 'next/router';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const AdminLayout = ({ children }: LayoutProps) => {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+    const handleRouteError = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleRouteStart);
+    router.events.on('routeChangeComplete', handleRouteDone);
+    router.events.on('routeChangeError', handleRouteError);
+
+    if (!user || !user.uid) {
+      NProgress.start();
+      router.push("/auth/logout").finally(() => NProgress.done());
+    }
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteStart);
+      router.events.off('routeChangeComplete', handleRouteDone);
+      router.events.off('routeChangeError', handleRouteError);
+    };
+  }, [user, router]);
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-200">
       <header>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import type { AppProps, AppType } from 'next/app';
 import { UserProvider } from '@/context/UserContext';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import React, { ReactNode } from 'react';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 interface MyAppProps extends AppProps {
   Component: React.ComponentType<any> & {
@@ -11,30 +13,26 @@ interface MyAppProps extends AppProps {
   };
 }
 
-export default class MyApp extends React.Component<MyAppProps> {
+const MyApp: AppType = ({ Component, pageProps }) => {
+  const Layout = Component.layout || (({ children }: { children: ReactNode }) => <>{children}</>);
 
-  static async getInitialProps({ Component, ctx }: any) {
-    let pageProps = {};
+  return (
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
+  );
+};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
+MyApp.getInitialProps = async ({ Component, ctx }: any) => {
+  let pageProps = {};
 
-    return { pageProps };
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
   }
 
-  render() {
-    const { Component, pageProps } = this.props;
-    const Layout = Component.layout || (({ children }: { children: ReactNode }) => <>{children}</>);
+  return { pageProps };
+};
 
-    return (
-      <>
-        <UserProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </UserProvider>
-      </>
-    );
-  }
-}
+export default MyApp;
